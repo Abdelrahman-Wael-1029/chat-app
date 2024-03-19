6373c466ca03f8bd9381747fad70b9b618922e6f
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2024-03-19 06:24:38 +0200

    update online state

--- lib/features/chat/repository/chat_repository.dart
+++ lib/features/chat/repository/chat_repository.dart
@@ -31,23 +31,23 @@ class ChatRepository {
         .snapshots()
         .asyncMap((event) async {
       List<ContactModel> contacts = [];
-      print(event.docs);
       for (var doc in event.docs) {
         var user = await store.collection('users').doc(doc.id).get();
         var contact = ContactModel.fromJson(user.data()!);
-        var lastMessageInfo = await store.collection('users').doc(auth.currentUser!.uid).collection('chat').doc(doc.id).get();
+        var lastMessageInfo = await store
+            .collection('users')
+            .doc(auth.currentUser!.uid)
+            .collection('chat')
+            .doc(doc.id)
+            .get();
         contact.lastMessage = lastMessageInfo['lastMessage'];
-        var time =DateTime.parse(lastMessageInfo['time']);
-        if(time.day == DateTime.now().day) {
+        var time = DateTime.parse(lastMessageInfo['time']);
+        if (time.day == DateTime.now().day) {
           contact.time = '${time.hour}:${time.minute}';
-        }
-          else if(time.day == DateTime.now().day - 1){
-            contact.time = 'Yesterday';
-          }
-          else {
-          contact.time =
-              time.day.toString() + '/' + time.month.toString() + '/' +
-                  time.year.toString();
+        } else if (time.day == DateTime.now().day - 1) {
+          contact.time = 'Yesterday';
+        } else {
+          contact.time = '${time.day}/${time.month}/${time.year}';
         }
         contacts.add(contact);
       }
@@ -80,49 +80,47 @@ class ChatRepository {
     required context,
     required MessageModel message,
   }) async {
-      await store.runTransaction((transaction) async {
-        await store
-            .collection('users')
-            .doc(message.senderId)
-            .collection('chat')
-            .doc(message.receiverId)
-            .collection('messages')
-            .add(message.toJson());
-        await store
-            .collection('users')
-            .doc(message.senderId)
-            .collection('chat')
-            .doc(message.receiverId)
-            .set({
-          'lastMessage': message.message,
-          'time': message.time,
-        });
-        await store
-            .collection('users')
-            .doc(message.receiverId)
-            .collection('chat')
-            .doc(message.senderId)
-            .collection('messages')
-            .add(message.toJson());
-
-        await store
-            .collection('users')
-            .doc(message.receiverId)
-            .collection('chat')
-            .doc(message.senderId)
-            .set({
-          'lastMessage': message.message,
-          'time': message.time,
-        });
-
-      }).catchError((e) {
-        showAwesomeDialog(
-          context,
-          dialogType: DialogType.error,
-          title: 'Error2',
-          desc: e.toString(),
-        );
+    await store.runTransaction((transaction) async {
+      await store
+          .collection('users')
+          .doc(message.senderId)
+          .collection('chat')
+          .doc(message.receiverId)
+          .collection('messages')
+          .add(message.toJson());
+      await store
+          .collection('users')
+          .doc(message.senderId)
+          .collection('chat')
+          .doc(message.receiverId)
+          .set({
+        'lastMessage': message.message,
+        'time': message.time,
       });
+      await store
+          .collection('users')
+          .doc(message.receiverId)
+          .collection('chat')
+          .doc(message.senderId)
+          .collection('messages')
+          .add(message.toJson());
 
+      await store
+          .collection('users')
+          .doc(message.receiverId)
+          .collection('chat')
+          .doc(message.senderId)
+          .set({
+        'lastMessage': message.message,
+        'time': message.time,
+      });
+    }).catchError((e) {
+      showAwesomeDialog(
+        context,
+        dialogType: DialogType.error,
+        title: 'Error2',
+        desc: e.toString(),
+      );
+    });
   }
 }
