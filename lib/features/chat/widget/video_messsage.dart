4ff811dad97c3c76e9089865fcff21f4b9a67351
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2024-03-22 10:22:49 +0200

    update show video

--- lib/features/chat/widget/video_messsage.dart
+++ lib/features/chat/widget/video_messsage.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
-import 'package:flick_video_player/flick_video_player.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../models/message.dart';
@@ -12,32 +14,41 @@ class VideoMessage extends StatefulWidget {
   @override
   _VideoMessageState createState() => _VideoMessageState();
 }
+// build using Chewie
 
 class _VideoMessageState extends State<VideoMessage> {
-  late FlickManager flickManager;
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
 
   @override
   void initState() {
     super.initState();
-    flickManager = FlickManager(
-      videoPlayerController: VideoPlayerController.networkUrl(
-        Uri.parse(widget.message.message),
-      ),
+    _videoPlayerController =
+        VideoPlayerController.networkUrl(Uri.parse(widget.message.message));
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      aspectRatio: 16 / 9,
       autoPlay: false,
+      looping: false,
+      showControls: true,
       autoInitialize: true,
+
     );
   }
 
   @override
   void dispose() {
-    flickManager.dispose();
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlickVideoPlayer(
-      flickManager: flickManager,
+    return SizedBox(
+      width: double.infinity,
+      height: max(200, MediaQuery.of(context).size.height * 0.3),
+      child: Chewie(controller: _chewieController),
     );
   }
 }
